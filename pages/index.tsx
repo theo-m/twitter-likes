f5c877d90589c2fa951c1da63bc63c606b59dc18
Théo Matussière <theo@matussie.re>
2022-10-25 13:07:10 +0200

    working explorer with embeded tweets

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import { ComponentType, useState } from "react";
 import { type TweetV2 } from "twitter-api-v2";
 import type * as icons from "@heroicons/react/24/outline";
 import dynamic from "next/dynamic";
+import Fuse from "fuse.js";
+import classNames from "classnames";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const HeroIcon = ({
   name,
@@ -20,8 +23,19 @@ const HeroIcon = ({
   return <Icon className={className} aria-hidden={true} />;
 };
 
+const fuse = new Fuse<TweetV2>([], {
+  keys: ["text"],
+  isCaseSensitive: false,
+  distance: 20,
+  includeScore: true,
+  shouldSort: true,
+});
+const pageSize = 30;
+
 export default function Home() {
   const [handle, setHandle] = useState<string>();
+  const [search, setSearch] = useState<string>();
+  const [results, setResults] = useState<Fuse.FuseResult<TweetV2>[]>([]);
   const [page, setPage] = useState(0);
   const likesQuery = useQuery(["likes", handle], {
     enabled: !!handle,
@@ -42,6 +56,10 @@ export default function Home() {
           });
         return r.json();
       }),
+    onSuccess: (d) => {
+      d.forEach((t) => fuse.add(t));
+      setResults(d.slice(0, 30).map((it) => ({ item: it, refIndex: 0 })));
+    },
   });
 
   return (
@@ -65,7 +83,10 @@ export default function Home() {
             onKeyDown={(e) =>
               e.key === "Enter" && setHandle(e.currentTarget.value)
             }
-            className="py-2 px-4 rounded-xl border"
+            className={classNames(
+              "py-2 px-4 rounded-xl border placeholder:text-gray-200",
+              handle && "bg-gray-100"
+            )}
           />
         </div>
         {likesQuery.isError && (
@@ -80,33 +101,85 @@ export default function Home() {
           </div>
         )}
         {likesQuery.isFetching && <div>Loading...</div>}
-        <div className="flex relative flex-col gap-4 max-w-[600px]">
-          {likesQuery.data?.slice(page, page + 100).map((it) => (
-            <div
-              key={it.id}
-              className="flex rounded-xl p-4 bg-white shadow-sm border flex-col gap-2"
-            >
-              <span className="text-black">{it.author_id}</span>
-              <p>{it.text}</p>
-              <a href={`https://twitter.com/twitter/status/${it.id}`}>
-                <HeroIcon name="LinkIcon" />
-              </a>
-            </div>
-          ))}
-          <div className="absolute bottom-4 right-4">
+        {likesQuery.data && (
+          <div>
+            Found{" "}
+            <span className="text-black font-bold">
+              {likesQuery.data.length.toLocaleString()}
+            </span>{" "}
+            tweets liked
+          </div>
+        )}
+        <div className="flex items-center gap-2">
+          <label className="font-bold text-black" htmlFor="search">
+            Search
+          </label>
+          <input
+            type="text"
+            name="search"
+            placeholder="border"
+            onChange={(e) =>
+              setSearch(() => {
+                const s = e.target.value ?? "";
+                const sd = fuse.search(s);
+                console.log(s, sd, e.target.value);
+                setResults(sd);
+                return s;
+              })
+            }
+            className="py-2 px-4 rounded-xl border placeholder:text-gray-200"
+          />
+        </div>
+
+        <div>
+          Found{" "}
+          <span className="text-black font-bold">
+            {results.length.toLocaleString()}
+          </span>{" "}
+          tweets liked matching &ldquo;
+          <span className="text-blue-400">{search ?? "-"}</span>&rdquo;
+        </div>
+
+        {results?.length > 0 && (
+          <div className="flex items-center gap-4">
             <button
-              onClick={() =>
-                setPage(
-                  Math.min(page + 100, (likesQuery.data?.length ?? 1e6) - 100)
-                )
-              }
+              className={classNames(
+                "text-white rounded-full px-4 py-1",
+                "focus:ring",
+                "bg-gray-500 hover:bg-opacity-80 disabled:bg-gray-200"
+              )}
+              disabled={page < pageSize}
+              onClick={() => setPage(page - pageSize)}
             >
-              <HeroIcon name="PlusCircleIcon" />
+              <HeroIcon name="ChevronLeftIcon" />
             </button>
-            <button onClick={() => setPage(Math.max(0, page - 100))}>
-              <HeroIcon name="MinusCircleIcon" />
+            <span>
+              {1 + Math.floor(page / pageSize)} /{" "}
+              {1 + Math.floor(results.length / pageSize)}
+            </span>
+            <button
+              className={classNames(
+                "text-white rounded-full px-4 py-1",
+                "focus:ring",
+                "bg-gray-500 hover:bg-opacity-80 disabled:bg-gray-200"
+              )}
+              disabled={page + pageSize >= results.length}
+              onClick={() => setPage(page + pageSize)}
+            >
+              <HeroIcon name="ChevronRightIcon" />
             </button>
           </div>
+        )}
+
+        <div className="flex relative flex-col gap-4 max-w-[600px]">
+          {results?.slice(page, page + pageSize).map(({ item: it, score }) => (
+            <div key={it.id} className="flex  p-4 w-[600px]  flex-col gap-2">
+              <TwitterTweetEmbed tweetId={it.id} placeholder={it.text} />
+              <span className="text-sm text-gray-200 font-bold">
+                Score: {score}
+              </span>
+            </div>
+          ))}
         </div>
       </main>
     </div>
