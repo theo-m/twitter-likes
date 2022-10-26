53b31430bdb8b217fd1318b727c94d0563293a57
Théo Matussière <theo@matussie.re>
2022-10-26 11:21:46 +0200

    more explicit about the intent

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,14 +6,17 @@ export default function About() {
     <div className="w-screen h-screen flex flex-col p-4 gap-16 items-center">
       <p className="prose sm:w-[480px]">
         This is a personal tiny tool to be able to browse the tweets I&apos;ve
-        personally liked.
+        personally liked - I&apos;m constantly vaguely remembering things that I
+        know I&apos;ve liked on Twitter and can&apos;t find them on their app
+        again.
         <br />
         The API key is subjected to a 75 query / 15min quota, which is quickly
         running out.
         <br />
         Almost everything happens in the browser: queries are cached and search
         happens thanks to fuse.js.
-        <br />
+      </p>
+      <p className="prose sm:w-[480px]">
         Feel free to{" "}
         <a
           href="https://github.com/theo-m/twitter-likes"
@@ -22,7 +25,7 @@ export default function About() {
         >
           fork the project
         </a>{" "}
-        to use it on your own.
+        to use it on your own with your own API key.
       </p>
       <div>
         <Link href="/">
